0b94c65c33c815d3c107622c8445f8c3d4d434ee
MarceloG2021 <marcelo.gamez2021@gmail.com>
2022-05-13 04:05:34 -0800

    React Sidebar Design

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 1 Se instala react-router 6 y se configura las rutas.
+# 1
+ Se instala react-router 6 y se configura las rutas.
+
+ # 2
+ se crea la barra de menu con los iconos de material UI(React Sidebar Design)
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
+import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.scss"
 
 const Home = () => {
   return (
     <div className="home">
-        home
+        <Sidebar/>
+        <div className="homeContainer">container</div>
     </div>
-  )
-}
+  );
+};
 
 
 export default Home
